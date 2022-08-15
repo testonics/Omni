3d67166ece47a4e0a5a9689705971111f5c1c831
nikhil2207 <nikhil.jain.2005@gmail.com>
2022-08-15 20:50:09 +0530

    Commented the code

--- src/test/java/TestngTest/Web/SimpleTest.java
+++ src/test/java/TestngTest/Web/SimpleTest.java
@@ -1,26 +1,26 @@
-package TestngTest.Web;
-
-import org.junit.Test;
-import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
-
-import web.Browser;
-
-public class SimpleTest {
-
-  @Test
-  public void sometest() {
-    //http://www.seleniumhq.org/download/maven.jsp
-    //https://code.google.com/p/selenium/wiki/GettingStarted
-    
-    WebDriver driver = Browser.launch();
-    driver.get("https://www.google.com");
-    WebElement element = driver.findElement(By.name("q"));
-    element.sendKeys("Cheese!");
-    element.submit();
-    System.out.println("Page title is: " + driver.getTitle());
-    driver.quit();
-  }
-  
-}
+//package TestngTest.Web;
+//
+//import org.junit.Test;
+//import org.openqa.selenium.By;
+//import org.openqa.selenium.WebDriver;
+//import org.openqa.selenium.WebElement;
+//
+//import web.Browser;
+//
+//public class SimpleTest {
+//
+//  @Test
+//  public void sometest() {
+//    //http://www.seleniumhq.org/download/maven.jsp
+//    //https://code.google.com/p/selenium/wiki/GettingStarted
+//
+//    WebDriver driver = Browser.launch();
+//    driver.get("https://www.google.com");
+//    WebElement element = driver.findElement(By.name("q"));
+//    element.sendKeys("Cheese!");
+//    element.submit();
+//    System.out.println("Page title is: " + driver.getTitle());
+//    driver.quit();
+//  }
+//
+//}
